5c0871f215825b6ac54f3744395c05625e46318f
MAYANK DUTT PATHAK <136955281+Mayankpathak04@users.noreply.github.com>
2024-07-20 12:16:27 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ function Footer() {
             <p>123 Main Street</p>
             <p>NewDelhi</p>
             <p>Phone: (123) 456-7890</p>
-            <p>Email: ujwalmogha2004@gmail.com</p>
+            <p>Email: mayankdutt.pathak@gmail.com</p>
           </div>
           <div className="w-full md:w-1/4 mb-4 md:mb-0">
             <h2 className="text-lg font-semibold mb-4">Quick Links</h2>
@@ -25,15 +25,15 @@ function Footer() {
           <div className="w-full md:w-1/4 mb-4 md:mb-0">
             <h2 className="text-lg font-semibold mb-4">Connect With Us</h2>
             <ul>
-              <li><a href="https://github.com/ujwalmogha04" target="_blank" rel="noopener noreferrer">Github</a></li>
+              <li><a href="https://github.com/Mayankpathak04" target="_blank" rel="noopener noreferrer">Github</a></li>
               <li><a href="https://twitter.com/example" target="_blank" rel="noopener noreferrer">Twitter</a></li>
               <li><a href="https://www.instagram.com/example" target="_blank" rel="noopener noreferrer">Instagram</a></li>
-              <li><a href="https://www.linkedin.com/in/ujwal-mogha" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+              <li><a href="https://www.linkedin.com/in/mayank-dutt-pathak/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
             </ul>
           </div>
         </div>
         <div className="border-t border-gray-800 mt-16 pt-9 text-center ">
-          <p className='mt-4'>&copy; {new Date().getFullYear()} Ujwal Mogha. All rights reserved.</p>
+          <p className='mt-4'>&copy; {new Date().getFullYear()} Mayank Dutt Pathak. All rights reserved.</p>
         </div>
       </div>
     </footer>
